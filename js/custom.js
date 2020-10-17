fd5c5f4a06e7490f2e60b039f7e5962c34b18d27
AlaminCoding <alamin.coding@gmail.com>
2020-10-17 17:47:43 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  var $tabs = $("#horizontalTab");
+  $tabs.responsiveTabs({
+    startCollapsed: "accordion",
+    collapsible: "accordion",
+  });
+});
+
+// const arrow = document.querySelectorAll(".down-arrow-img");
+// arrow.forEach((element, index) => {
+//   console.log(element);
+//   element.addEventListener("click", function () {
+//     element.style.display = "none";
+//   });
+// });
+// console.log(arrow);
+new Glide(".glide", {
+  type: "carousel",
+  startAt: 0,
+  perView: 2,
+  breakpoints: {
+    800: {
+      perView: 1,
+    },
+  },
+}).mount();
